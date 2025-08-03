8e739bda892dfc633a25a28e38441665ed208ae7
raoneinet <raoneinet@hotmail.com>
2025-08-03 11:49:07 +0100

    adding icon

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,7 +1,12 @@
-export const Header = ()=>{
+export const Header = () => {
     return (
-        <div className="w-full h-30 bg-gray-700 text-white p-3">
-            <h1 className="text-4xl font-bold">Morse Code Translator</h1>
+        <div className="w-full flex items-center h-30 bg-gray-700 text-white p-3">
+            <div>
+                <img src="/morse.png" className="w-20" />
+            </div>
+            <div>
+                <h1 className="text-4xl font-bold">ABCM-.</h1>
+            </div>
         </div>
     )
 }
